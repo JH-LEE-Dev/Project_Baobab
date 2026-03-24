ab92a73c8da6b30c0a493823393e6528d63a0fa8
VOID <void91112@gmail.com>
2026-03-24 11:44:47 +0900

    🔶 데미지 입은 나무 중복 처리 해결 중

--- Assets/Scripts/Presentation Layer/UISystem/UIView/Unit/UIView_Unit.cs
+++ Assets/Scripts/Presentation Layer/UISystem/UIView/Unit/UIView_Unit.cs
@@ -18,7 +18,7 @@ namespace PresentationLayer.UISystem.View
         [SerializeField] private GameObject hpBarPrefab;
         private ObjectPools hpBarPool;
         
-        private List<ITreeObj> damagedTrees = new List<ITreeObj>(32);
+        private Dictionary<ITreeObj, HUD_ProgressBar> hitTrees = new Dictionary<ITreeObj, HUD_ProgressBar>(32);
 
         // //퍼블릭 초기화 및 제어 메서드
 
@@ -41,10 +41,21 @@ namespace PresentationLayer.UISystem.View
         public void TreeGetHit(ITreeObj _treeObj)
         {
             if (null == _treeObj)
+            {
                 return;
-            
-            damagedTrees.Add(_treeObj);
-            //OnShow_HPBar(_treeObj);
+            }
+
+            if (hitTrees.TryGetValue(_treeObj, out HUD_ProgressBar _bar))
+            {
+                // 이미 HP 바가 떠 있는 경우: 값 업데이트 및 시간 연장
+                _bar.UpdateValue(_treeObj.health.GetCurrentHealth());
+                _bar.TriggerActiveForDuration(3.0f, FinishedBar);
+            }
+            else
+            {
+                // 새로 생성하여 노출
+                OnShow_HPBar(_treeObj);
+            }
         }
 
         public void DependencyInjection(IReadOnlyList<ITreeObj> _trees)
@@ -65,11 +76,38 @@ namespace PresentationLayer.UISystem.View
             _bar.UpdateValue(_treeObj.health.GetCurrentHealth());
             _bar.UpdateTargetObj(_treeObj.GetTransform().gameObject);
             
+            // 딕셔너리에 등록 (중복 체크는 TreeGetHit에서 수행함)
+            hitTrees.Add(_treeObj, _bar);
+
             // 3초 동안 활성화하고, 종료 시 풀에 반납하도록 콜백 등록
             _bar.TriggerActiveForDuration(3.0f, FinishedBar);
         }
 
-        private void FinishedBar(HUD_ProgressBar _bar) => hpBarPool?.Despawn(_bar.gameObject);
+        private void FinishedBar(HUD_ProgressBar _bar)
+        {
+            if (null == _bar)
+            {
+                return;
+            }
+
+            // 해당 바를 사용 중이던 나무를 찾아 딕셔너리에서 제거
+            ITreeObj _ownerTree = null;
+            foreach (var _kvp in hitTrees)
+            {
+                if (_kvp.Value == _bar)
+                {
+                    _ownerTree = _kvp.Key;
+                    break;
+                }
+            }
+
+            if (null != _ownerTree)
+            {
+                hitTrees.Remove(_ownerTree);
+            }
+
+            hpBarPool?.Despawn(_bar.gameObject);
+        }
 
         // //유니티 이벤트 함수
 
