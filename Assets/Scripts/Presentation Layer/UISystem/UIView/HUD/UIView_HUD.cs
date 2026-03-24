54d835400f98cea2fabeaa0e219df4c36e906ee7
VOID <void91112@gmail.com>
2026-03-24 15:06:05 +0900

    🔶 SteminaBar 작업 중

--- Assets/Scripts/Presentation Layer/UISystem/UIView/HUD/UIView_HUD.cs
+++ Assets/Scripts/Presentation Layer/UISystem/UIView/HUD/UIView_HUD.cs
@@ -1,4 +1,5 @@
 using System;
+using PresentationLayer.UISystem.HUD;
 using UnityEngine;
 
 public class UIView_HUD : UIView
@@ -6,13 +7,19 @@ public class UIView_HUD : UIView
     [Header("UI References")]
     [SerializeField] private Transform uiRoot; //일단 에디터에서 자기 자신 넣으면 됨.
     [SerializeField] private GameObject hudEquipmentPrefab;
+    [SerializeField] private GameObject hudSteminaBarPrefab;
 
     private HUD_Equipment hudEquipment;
+    private HUD_ProgressBar hudSteminaBar;
+
+#region Default Logic
 
     public override void Initialize(UIViewContext _ctx)
     {
         base.Initialize(_ctx);
+
         Init_HUDEquipment();
+        Init_HUDSteminaBar();
     }
 
     public override void OnDestroy()
@@ -34,6 +41,10 @@ public class UIView_HUD : UIView
         base.OnHide();
     }
 
+#endregion
+
+#region HUD_Equipment Logic
+
     private void Init_HUDEquipment()
     {
         hudEquipment = Instantiate(hudEquipmentPrefab, this.transform).GetComponent<HUD_Equipment>();
@@ -41,4 +52,25 @@ public class UIView_HUD : UIView
         if (null != hudEquipment)
             hudEquipment.Initialize();
     }
+
+#endregion
+    
+#region HUD_Stemina Logic
+
+    private void Init_HUDSteminaBar()
+    {
+        hudSteminaBar = Instantiate(hudSteminaBarPrefab, this.transform).GetComponent<HUD_ProgressBar>();
+
+        if (null != hudSteminaBar)
+            hudSteminaBar.Initialize();
+    }
+
+    private void UsedSteminaEvent(float _currentStemina, float _maxStemina)
+    {
+        float newRatio = _currentStemina / _maxStemina;
+        hudSteminaBar?.UpdateValue(newRatio);
+    }
+
+#endregion
+
 }
