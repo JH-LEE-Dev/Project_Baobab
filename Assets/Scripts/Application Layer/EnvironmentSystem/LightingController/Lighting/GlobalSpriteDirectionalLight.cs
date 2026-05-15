8f4653a590c527e905d68c16479981e8c2e1ad33
messier3331 <messier3331@naver.com>
2026-05-15 18:30:33 +0900

    머테리얼 전부 수정
    ㅇ

--- Assets/Scripts/Application Layer/EnvironmentSystem/LightingController/Lighting/GlobalSpriteDirectionalLight.cs
+++ Assets/Scripts/Application Layer/EnvironmentSystem/LightingController/Lighting/GlobalSpriteDirectionalLight.cs
@@ -53,6 +53,7 @@ public class GlobalSpriteDirectionalLight : MonoBehaviour
 
     [Header("Point Lights")]
     [SerializeField, Range(0, MaxPointLights)] private int maxPointLights = MaxPointLights;
+    [SerializeField, Min(0.1f)]
     private float objectPointLightRangeMultiplier = 2.5f;
 
     [Header("External Inputs")]
