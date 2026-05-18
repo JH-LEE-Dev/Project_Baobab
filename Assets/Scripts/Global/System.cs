0f0df9541a36c81196224423f590f867256dc2ce
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-05-18 19:12:03 +0900

    구름 밀도 증가

--- Assets/Scripts/Global/System.cs
+++ Assets/Scripts/Global/System.cs
@@ -7,5 +7,5 @@ public struct SYSTEM_VAR
 
     public const int MAX_ANIMAL_CNT = 100;
     public const int MAX_UI_HPBAR_CNT = 30;
-    public const int MAX_ENV_OBJ_CNT = 200;
+    public const int MAX_ENV_OBJ_CNT = 1000;
 }
