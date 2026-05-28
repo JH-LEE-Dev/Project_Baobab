209eca94c19ccb40cae9a367c3fc8a125c6ba4cd
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-05-28 16:23:59 +0900

    아이템 그림자 수정

---
+++ Assets/Scripts/Application Layer/ObjectSystem/Item/Item/Coin.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer sr;
+    [SerializeField] private Animator anim;
+    
+    public void Initailize()
+    {
+        
+    }
+}
