693699dc797163ef5d21f99344bb58c7dbf574bd
JH-LEE-Dev <140565804+JH-LEE-Dev@users.noreply.github.com>
2026-08-25 17:37:16 +0900

    데모 전용 HP 속성 적용

--- Assets/Scripts/Application Layer/Scriptable Objects/TreeStatDataBase/TreeStatDataBase.cs
+++ Assets/Scripts/Application Layer/Scriptable Objects/TreeStatDataBase/TreeStatDataBase.cs
@@ -7,9 +7,24 @@ public class TreeStatDataBase : ScriptableObject
 {
     public List<TreeStatData> treeStatDatas;
 
+    [Header("데모 빌드 전용 HP 오버라이드")]
+    [Tooltip("BuildInfo.IsDemo가 true일 때만 적용됩니다. 여기 등록된 TreeType은 기본 hp 대신 이 값을 사용합니다.")]
+    public List<TreeDemoHpOverrideData> demoHpOverrides;
+
     public TreeStatData Get(TreeType _type)
     {
-        return treeStatDatas.Find(x => x.treeType == _type);
+        TreeStatData data = treeStatDatas.Find(x => x.treeType == _type);
+
+        if (BuildInfo.IsDemo && demoHpOverrides != null)
+        {
+            int overrideIndex = demoHpOverrides.FindIndex(x => x.treeType == _type);
+            if (overrideIndex >= 0)
+            {
+                data.hp = demoHpOverrides[overrideIndex].hp;
+            }
+        }
+
+        return data;
     }
 }
 
@@ -21,6 +36,13 @@ public struct TreeStatData
     public float sp;
     public float spRegen;
     public SPRegenStrategySO regenStrategy;
+}
+
+[Serializable]
+public struct TreeDemoHpOverrideData
+{
+    public TreeType treeType;
+    public float hp;
 }
 
 [Serializable]
